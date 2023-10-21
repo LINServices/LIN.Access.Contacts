88778bd4a56b6ea25d935c195fbc45465fbb5ea3
Alex <giraldojhong4@gmail.com>
2023-10-21 17:08:38 -0500

    Acceso a Emma

--- Controllers/Profiles.cs
+++ Controllers/Profiles.cs
@@ -7,6 +7,55 @@ public static class Profiles
 {
 
 
+
+    /// <summary>
+    /// Obtiene las conversaciones asociadas a un perfil
+    /// </summary>
+    /// <param name="token">Token de acceso</param>
+    public async static Task<ReadOneResponse<Message>> ToEmma(string modelo, string token)
+    {
+
+        // Crear HttpClient
+        using var httpClient = new HttpClient();
+        httpClient.DefaultRequestHeaders.Add("token", token);
+
+
+        // ApiServer de la solicitud GET
+        string url = ApiServer.PathURL("emma");
+        var json = JsonConvert.SerializeObject(modelo);
+
+        try
+        {
+            // Contenido
+            StringContent content = new(json, Encoding.UTF8, "application/json");
+
+            // Envía la solicitud
+            var response = await httpClient.PostAsync(url, content);
+
+            // Lee la respuesta del servidor
+            var responseContent = await response.Content.ReadAsStringAsync();
+
+            var obj = JsonConvert.DeserializeObject<ReadOneResponse<Message>>(responseContent);
+
+            return obj ?? new();
+
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Error al hacer la solicitud GET: {e.Message}");
+        }
+
+
+        return new();
+
+
+
+
+
+    }
+
+
+
     /// <summary>
     /// Iniciar sesión
     /// </summary>
---
+++ MM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LIN.Access.Contacts;
+
+
+
+
+public class Message
+{
+    public string Result { get; set; }
+    public MessageTypes Type { get; set; }
+}
+
+public enum MessageTypes
+{
+    Text,
+    Command
+}
