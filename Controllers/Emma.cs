d17bf103ba7a2dbf7bf89a80a8f019ba4f5ca0ad
Alexander Giraldo <giraldojhong4@gmail.com>
2024-11-04 17:37:07 -0500

    Integracion IA V2

--- Controllers/Emma.cs
+++ Controllers/Emma.cs
@@ -6,12 +6,11 @@ namespace LIN.Access.Contacts.Controllers;
 public class Emma
 {
 
-
     /// <summary>
     /// Obtiene las conversaciones asociadas a un perfil
     /// </summary>
     /// <param name="token">Token de acceso</param>
-    public static async Task<ReadOneResponse<AssistantResponse>> ToEmma(string modelo, string token)
+    public static async Task<ReadOneResponse<Types.Cloud.OpenAssistant.Models.EmmaSchemaResponse>> ToEmma(string modelo, string token)
     {
 
         // Cliente
@@ -20,10 +19,8 @@ public class Emma
         // Headers.
         client.AddHeader("tokenAuth", token);
 
-        return await client.Post<ReadOneResponse<AssistantResponse>>(modelo);
+        return await client.Post<ReadOneResponse<LIN.Types.Cloud.OpenAssistant.Models.EmmaSchemaResponse>>(modelo);
 
     }
-
-
 
 }
